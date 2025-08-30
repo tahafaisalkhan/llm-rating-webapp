15de373e4b42840d5cca44026c27191c56755a8d
tahafaisalkhan <tahafaisal999@gmail.com>
2025-08-30 21:18:46 +0500

    Revert "update"
    This reverts commit 25b7161f653c11d76ae41fd8db978685cdc2fce3.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 // api/index.js
 const serverless = require("serverless-http");
-const createServer = require("../server/index.express"); // <- see next file
+const path = require("path");
+
+// Load your existing Express server app:
+require("dotenv").config(); // Vercel also injects envs, this is fine locally
+const createServer = require("../server/index.express");
+// ^ We'll create this tiny exporter in step 2
 
 const app = createServer();
+
 module.exports = serverless(app);
