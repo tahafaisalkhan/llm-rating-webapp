25b7161f653c11d76ae41fd8db978685cdc2fce3
tahafaisalkhan <tahafaisal999@gmail.com>
2025-08-30 17:43:03 +0500

    update

--- api/index.js
+++ api/index.js
@@ -1,12 +1,6 @@
 // api/index.js
 const serverless = require("serverless-http");
-const path = require("path");
-
-// Load your existing Express server app:
-require("dotenv").config(); // Vercel also injects envs, this is fine locally
-const createServer = require("../server/index.express");
-// ^ We'll create this tiny exporter in step 2
+const createServer = require("../server/index.express"); // <- see next file
 
 const app = createServer();
-
 module.exports = serverless(app);
