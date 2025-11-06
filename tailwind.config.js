86aacb0aa8b9dc801061ccdc12782b1b9ab60c1f
tahafaisalkhan <tahafaisal999@gmail.com>
2025-11-07 00:56:53 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       fontFamily: {
-        nastaliq: ["'Noto Nastaliq Urdu'", "serif"],
+        nastaliq: ["Noto Nastaliq Urdu", "serif"],
       },
     },
   },
